926dd3e06809f89eff7d29e1c03be8c3993e3763
Gavin Panella <gavinpanella@gmail.com>
2020-01-10 15:23:21 +0100

    Fix some punctuation.

--- src/cmds/hook/stale.sh
+++ src/cmds/hook/stale.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=bash
 log_status "$(warning WARNING): $(em 'Nix environment is out of date!') " >&2
-log_status "--> Use $(em firstaide build) to rebuild it" >&2
+log_status "--> Use $(em firstaide build) to rebuild it." >&2
 log_status "$(warning WARNING): Loading $(em STALE) environment ;-(" >&2
