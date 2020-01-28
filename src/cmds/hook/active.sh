2aed5e702f82e77cb71957d80f91d6ee3250a7c4
mavnn <michael@mavnn.co.uk>
2020-01-28 16:36:24 +0000

    Allow active hint message to be overridden

--- src/cmds/hook/active.sh
+++ src/cmds/hook/active.sh
@@ -1,5 +1,7 @@
 # shellcheck shell=bash
+MESSAGE=${FIRSTAIDE_ACTIVE_MESSAGE:-"aide build help"}
+UNDERLINE=${MESSAGE//?/^}
 log_status "$(okay OKAY): $(em 'Nix environment is up-to-date!')" >&2
 log_status "This is a $(em minimal environment); subprojects may not be built." >&2
-log_status "--> Use $(em aide build help) to find out what to do next." >&2
-log_status "        $(em ^^^^^^^^^^^^^^^)" >&2
+log_status "--> Use $(em $MESSAGE) to find out what to do next." >&2
+log_status "        $(em $UNDERLINE)" >&2
