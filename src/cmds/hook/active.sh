f82a59870fc3c741f78f6eec2d0e117d81399f4b
Gavin Panella <gavinpanella@gmail.com>
2020-01-28 21:25:06 +0100

    Interpolate the getting started message into the active hook.

--- src/cmds/hook/active.sh
+++ src/cmds/hook/active.sh
@@ -1,7 +1,5 @@
 # shellcheck shell=bash
-MESSAGE=${FIRSTAIDE_ACTIVE_MESSAGE:-"aide build help"}
-UNDERLINE=${MESSAGE//?/^}
 log_status "$(okay OKAY): $(em 'Nix environment is up-to-date!')" >&2
 log_status "This is a $(em minimal environment); subprojects may not be built." >&2
-log_status "--> Use $(em $MESSAGE) to find out what to do next." >&2
-log_status "        $(em $UNDERLINE)" >&2
+log_status "--> Use $(em __MESSAGE__) to find out what to do next." >&2
+log_status "        $(m=__MESSAGE__ && em "${m//?/^}")" >&2
