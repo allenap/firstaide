c3f9949176cc2c54b6324da75db6b05d84bc692b
Gavin Panella <gavinpanella@gmail.com>
2020-02-08 11:11:07 +0100

    Lower default logging level, and allow for --verbose/--quiet flags.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,31 @@ mod status;
 mod sums;
 
 fn main() {
-    if let Err(err) = init() {
-        eprintln!("{}", err);
-        process::exit(2);
-    };
+    // A note on logging. I don't like how logging works right now. It's not
+    // bad, but it's not good either. However, it does work. So that I can
+    // change my mind in the future, there's minimal UX to influence logging:
+    // just a --verbose and a --quiet flag, which cannot be used together. No
+    // short -v/-q flags, no multiple flags to incrementally increase or
+    // decrease verbosity, and no fancy formatting.
 
     let matches = clap::App::new("firstaide")
         .version(crate_version!())
         .author(crate_authors!())
         .about("First, as in prior to, aide.")
+        .arg(
+            clap::Arg::with_name("verbose")
+                .long("verbose")
+                .global(true)
+                .help("Be more verbose")
+                .conflicts_with("quiet"),
+        )
+        .arg(
+            clap::Arg::with_name("quiet")
+                .long("quiet")
+                .global(true)
+                .help("Be quieter")
+                .conflicts_with("verbose"),
+        )
         .subcommand(cmds::build::argspec())
         .subcommand(cmds::status::argspec())
         .subcommand(cmds::clean::argspec())
@@ -30,6 +46,18 @@ fn main() {
         .setting(clap::AppSettings::DeriveDisplayOrder)
         .setting(clap::AppSettings::SubcommandRequired)
         .get_matches();
+
+    let log_level = if matches.is_present("verbose") {
+        log::LevelFilter::Debug
+    } else if matches.is_present("quiet") {
+        log::LevelFilter::Warn
+    } else {
+        log::LevelFilter::Info
+    };
+    if let Err(err) = init(log_level) {
+        eprintln!("{}", err);
+        process::exit(2);
+    };
 
     use error::Error::*;
     let result: Result<u8, error::Error> = match matches.subcommand() {
@@ -54,7 +82,7 @@ fn main() {
     };
 }
 
-fn init() -> Result<(), log::SetLoggerError> {
+fn init(log_level: log::LevelFilter) -> Result<(), log::SetLoggerError> {
     fern::Dispatch::new()
         // Perform allocation-free log formatting.
         .format(|out, message, record| {
@@ -67,7 +95,7 @@ fn init() -> Result<(), log::SetLoggerError> {
             ))
         })
         // Add blanket level filter.
-        .level(log::LevelFilter::Debug)
+        .level(log_level)
         // Output to stderr.
         .chain(std::io::stderr())
         // Apply globally.
