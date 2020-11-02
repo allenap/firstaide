81e4fc32edbcf8b4f3a192dc867a80a06c7c39f4
Brian Hicks <brian@brianthicks.com>
2020-11-02 10:12:53 -0600

    get a nix shell for development

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+
+  nixpkgs = import sources.nixpkgs { };
+
+  niv = import sources.niv { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "firstaide";
+  buildInputs = [ niv.niv git pkgs.rustc ];
+}
