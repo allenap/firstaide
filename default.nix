dab42471a7ed2d0122e79a7b90a29d2cb58e4b68
Brian Hicks <brian@brianthicks.com>
2020-11-02 12:30:27 -0600

    Revert "Merge pull request #2 from NoRedInk/naersk"
    This reverts commit 144b247abf31755a4c8a653eb6d5e7badc9e14a6, reversing
    changes made to a9c6d0f5fb0efad0d99df695c41b11b89b1b3d60.

--- default.nix
+++ default.nix
@@ -3,5 +3,28 @@ with pkgs;
 let
   sources = import ./nix/sources.nix;
   gitignore = import sources.gitignore { };
-  naersk = pkgs.callPackage sources.naersk { };
-in naersk.buildPackage (gitignore.gitignoreSource ./.)
+in rustPlatform.buildRustPackage rec {
+  pname = "firstaide";
+  version = "0.1.6";
+  src = gitignore.gitignoreSource ./.;
+
+  # The crypto_hash crate needs the openssl-sys crate (directly or indirectly,
+  # I don't know) which ultimately needs openssl proper, and pkg-config.
+  buildInputs = [ openssl pkg-config ];
+
+  # Don't run tests when building.
+  doCheck = false;
+
+  # I think this refers to the current state of the crates.io repo. To update,
+  # replace the hash with all 0's and Nix will give you the right value to
+  # stick in here.
+  cargoSha256 = "1766w77b582c6j2qgqbfy652nxr90kgqw72fdwyd8mb3wjxxi35p";
+
+  meta = with stdenv.lib; {
+    description = "Bootstrap Nix environments.";
+    homepage = "https://github.com/allenap/firstaide";
+    license = with licenses; [ asl20 ];
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
