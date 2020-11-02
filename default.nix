e8a8af4c70575a84a4dd8865e05d0706e4734418
Brian Hicks <brian@brianthicks.com>
2020-11-02 10:23:15 -0600

    move gitignore source to niv

--- default.nix
+++ default.nix
@@ -1,19 +1,12 @@
-with import <nixpkgs> { };
-
+{ pkgs ? import <nixpkgs> { }, ... }:
+with pkgs;
 let
-  # A handy tool to correctly process .gitignore files.
-  gitignoreSrc = pkgs.fetchFromGitHub {
-    owner = "hercules-ci";
-    repo = "gitignore";
-    rev = "7415c4feb127845553943a3856cbc5cb967ee5e0";
-    sha256 = "sha256:1zd1ylgkndbb5szji32ivfhwh04mr1sbgrnvbrqpmfb67g2g3r9i";
-  };
-  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-
+  sources = import ./nix/sources.nix;
+  gitignore = import sources.gitignore { };
 in rustPlatform.buildRustPackage rec {
   pname = "firstaide";
   version = "0.1.5";
-  src = gitignoreSource ./.;
+  src = gitignore.gitignoreSource ./.;
 
   # The crypto_hash crate needs the openssl-sys crate (directly or indirectly,
   # I don't know) which ultimately needs openssl proper, and pkg-config.
