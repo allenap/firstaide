fa159eac0bd9e46bd5b15681e350b5409de7907b
Ju Liu <ju@noredink.com>
2021-02-04 15:55:28 +0000

    Tell people to use the safe version

--- src/cmds/hook/inactive.sh
+++ src/cmds/hook/inactive.sh
@@ -1,3 +1,3 @@
 # shellcheck shell=bash
 log_status "$(error ERROR): $(em 'Nix environment is not yet built!')" >&2
-log_status "--> Use $(em firstaide build) to build it." >&2
+log_status "--> Use $(em ./firstaide-build) to build it." >&2
